50016476564208ffb80c65e1e23c0ff98d539e40
Aluno <jhenni.bueno@gmail.comgit config user.email jhenni.bueno@gmail.com>
2024-08-15 19:23:57 -0300

    aula1

---
+++ aula1.js
@@ -0,0 +1,27 @@
+//ecmascript 6 -->2015
+
+
+let nome = "Jhennifer"; //string
+let idade = 12; //number - int
+let peso = 43.7; //number -float
+let acordado = true; //boolean
+
+let cidades = ["Curitiba","Pinhais", "Colombo"];  // Array
+
+
+if (nome == idade)
+{
+    console.log ("verdadeiro");    
+}else
+{
+    console.log("falso")    
+}
+
+function soma (n1 ,n2)
+{
+    let res = n1+n2;
+    return res;
+}
+
+let valor = soma (3,5);
+console.log(valor);
