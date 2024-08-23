80495b7b89bb68aa8ee4ceaca2ebd9b7eaa72a14
jhennifer <jhenni.bueno@gmail.com>
2024-08-22 21:06:51 -0300

    Crud

--- crud.js
+++ crud.js
@@ -3,3 +3,64 @@
 // Read -> listar
 // Update -> atualização
 // Delete -> apagar
+
+const { MongoClient ,ObjectId} = require("mongodb");
+
+constfs = require("fs");
+
+const urlcon = "mongodb+srv://aula-back:aula123456@turma-agosto.mam4s.mongodb.net/"
+
+const conexao = new MongoClient(urlcon);
+
+async function lerDados()
+{
+    let db = await conexao.connect();
+    let pasta = db.db("jhennifer").collection("contatos");
+    let dados = await pasta.find({}).toArray();
+    await db.close();
+
+    console.log(dados);
+
+}
+async function cadastrar()
+{
+    let db = await conexao.connect();
+    let pasta = db.db("jhennifer").collection("contatos");
+
+    let pessoa = 
+    {
+        nome: "Wesley",
+        email: "wesley.safadao@gmail.com",
+        cidade: "Ponta Grossa",
+    };
+    let retorno = await pasta.insertOne (pessoa)
+    await db.close();
+
+    console.log(retorno);
+
+}
+
+async function atualizar()
+{
+    let db = await conexao .connect();
+    let pasta = db.db("jhennifer").collection("contatos");
+    let pessoa = {_id: new ObjectId('66c685a3018b31579dd848cd')};
+    let valor = {email: "tereza.ferrer@gmail.com"};
+    let retorno = await pasta.updateOne (pessoa, {$set: valor});
+    db.close();
+   
+};
+
+async function deletar()
+{
+    let db = await conexao .connect();
+    let pasta = db.db("jhennifer").collection("contatos");
+
+    let pessoa = {_id: new ObjectId('66c685a3018b31579dd848cd')};
+    let retorno = await pasta.deleteOne (pessoa);
+
+    db.close();
+    console.log (retorno);
+
+}
+deletar();
